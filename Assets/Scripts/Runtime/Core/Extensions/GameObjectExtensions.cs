ea61e60fa3bcd827ae86a39a5e3257487376d974
TarobeWanwanLand <99389252+TarobeWanwanLand@users.noreply.github.com>
2023-06-11 22:46:14 +0900

    [remove] 旧WebRequestクラスの削除

---
+++ Assets/Scripts/Runtime/Core/Extensions/GameObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Pets.Core.Runtime.Core.Extensions
+{
+    public static class GameObjectExtensions
+    {
+        /// <summary>
+        /// ゲームオブジェクトがプレハブかどうかを判定する
+        /// </summary>
+        /// <param name="gameObject">判定するゲームオブジェクト</param>
+        /// <returns>プレハブであれば真</returns>
+        public static bool IsPrefab(this GameObject gameObject)
+        {
+            return gameObject.scene == default;
+        }
+    }
+}
