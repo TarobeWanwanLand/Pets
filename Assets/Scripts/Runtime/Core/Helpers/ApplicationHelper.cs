e26f1c4d7639768a76890058e82c92693f2e54a5
TarobeWanwanLand <99389252+TarobeWanwanLand@users.noreply.github.com>
2023-06-11 22:41:42 +0900

    [update] ApplicationHelperのリファクタリング

--- Assets/Scripts/Runtime/Core/Helpers/ApplicationHelper.cs
+++ Assets/Scripts/Runtime/Core/Helpers/ApplicationHelper.cs
@@ -1,7 +1,14 @@
 ﻿namespace Pets.Core.Helpers
 {
+    /// <summary>
+    /// アプリケーションのヘルパークラス
+    /// </summary>
     public static class ApplicationHelper
     {
+        /// <summary>
+        /// アプリケーションを終了する
+        /// </summary>
+        /// <remarks>エディタ上ではプレイモードを終了する</remarks>
         public static void Quit()
         {
 #if UNITY_EDITOR
